5ef20ff8ccd09b42981f783f2f4ba755ea018e3c
Roy Hwang <tupaclypse@gmail.com>
2016-12-13 13:34:52 -0800

    Backend done, empty view ejs files, seasons 1-4 done

--- controllers/pages.js
+++ controllers/pages.js
@@ -18,11 +18,58 @@ var harold = function(req, res, next){
   res.render('pages/harold');
 };
 
+var ilan = function(req, res, next){
+  res.render('pages/ilan');
+};
+
+var hung = function(req, res, next){
+  res.render('pages/hung');
+};
+
+var stephanie = function(req, res, next){
+  res.render('pages/stephanie');
+};
+
+var hosea = function(req, res, next){
+  res.render('pages/hosea');
+};
+var michael = function(req, res, next){
+  res.render('pages/michael');
+};
+var kevin = function(req, res, next){
+  res.render('pages/kevin');
+};
+var richard = function(req, res, next){
+  res.render('pages/richard');
+};
+var paul = function(req, res, next){
+  res.render('pages/paul');
+};
+var kristen = function(req, res, next){
+  res.render('pages/kristen');
+};
+var nicholas = function(req, res, next){
+  res.render('pages/nicholas');
+};
+var mei = function(req, res, next){
+  res.render('pages/mei');
+};
 
 module.exports = {
   welcome: welcome,
   chefs: chefs,
   disclaimer: disclaimer,
   about: about,
-  harold: harold
+  harold: harold,
+  ilan: ilan,
+  hung: hung,
+  stephanie: stephanie,
+  hosea: hosea,
+  michael: michael,
+  kevin: kevin,
+  richard: richard,
+  paul: paul,
+  kristen: kristen,
+  nicholas: nicholas,
+  mei: mei
 };
